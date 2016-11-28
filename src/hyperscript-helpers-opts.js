b5ddefb541d275824bc11143e1bda457f35a3770
chris <chris@bumblehead.com>
2016-11-27 23:50:39 -0800

    remove locally already-defined TAG_NAMES --use TAG_NAMES defined in library

--- src/hyperscript-helpers-opts.js
+++ src/hyperscript-helpers-opts.js
@@ -1,54 +1,20 @@
 // Filename: hyperscript-helpers-opts.js  
-// Timestamp: 2016.11.27-23:08:53 (last modified)
+// Timestamp: 2016.11.27-23:49:47 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>  
 
 let hh = require('hyperscript-helpers'),
     hhsvg = require('hyperscript-helpers/dist/svg');
 
 let hyperscripthelpersopts = module.exports = (o => {
-  
-  const TAG_NAMES = [
-    'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b',
-    'base', 'basefont', 'bdi', 'bdo', 'bgsound', 'big', 'blink', 'blockquote', 'body',
-    'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup',
-    'command', 'content', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog',
-    'dir', 'div', 'dl', 'dt', 'element', 'em', 'embed', 'fieldset', 'figcaption', 'figure',
-    'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-    'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input',
-    'ins', 'isindex', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'listing', 'main',
-    'map', 'mark', 'marquee', 'math', 'menu', 'menuitem', 'meta', 'meter', 'multicol',
-    'nav', 'nextid', 'nobr', 'noembed', 'noframes', 'noscript', 'object', 'ol', 'optgroup',
-    'option', 'output', 'p', 'param', 'picture', 'plaintext', 'pre', 'progress', 'q', 'rb',
-    'rbc', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'shadow',
-    'small', 'source', 'spacer', 'span', 'strike', 'strong', 'style', 'sub', 'summary',
-    'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead',
-    'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr', 'xmp'
-  ];
-
-  const TAG_NAMES_SVG = [
-    'a', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate', 'animateColor',
-    'animateMotion', 'animateTransform', 'circle', 'clipPath', 'colorProfile',
-    'cursor', 'defs', 'desc', 'ellipse', 'feBlend', 'feColorMatrix',
-    'feComponentTransfer', 'feComposite', 'feConvolveMatrix', 'feDiffuseLighting',
-    'feDisplacementMap', 'feDistantLight', 'feFlood', 'feFuncA', 'feFuncB',
-    'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode',
-    'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting',
-    'feSpotlight', 'feTile', 'feTurbulence', 'filter', 'font', 'fontFace',
-    'fontFaceFormat', 'fontFaceName', 'fontFaceSrc', 'fontFaceUri',
-    'foreignObject', 'g', 'glyph', 'glyphRef', 'hkern', 'image', 'line',
-    'linearGradient', 'marker', 'mask', 'metadata', 'missingGlyph', 'mpath',
-    'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect', 'script',
-    'set', 'stop', 'style', 'switch', 'symbol', 'text', 'textPath', 'title',
-    'tref', 'tspan', 'use', 'view', 'vkern'
-  ];
-
-    
   // var div = h('div.hello/world#hello/world');
   // div.properties.className; // hello
   // div.properties.id;        // hello
   
-  const encodeid = idstr => idstr.replace(/\//g, ':');
-  const decodeid = idstr => idstr.replace(/\:/g, '/');
+  const encodeid = idstr =>
+          idstr.replace(/\//g, ':');
+  
+  const decodeid = idstr =>
+          idstr.replace(/\:/g, '/');
   
   const getoptsclassidstr = (opts, classidstr) => (
     encodeid(classidstr.replace(/:[^: -.#]*/g, m => {
@@ -58,10 +24,8 @@ let hyperscripthelpersopts = module.exports = (o => {
     }))
   );
 
-  const buildoptfns = (h, helpers, tagnamearr) => {
-    let helperfns = helpers(h);
-
-    return tagnamearr.reduce((hhh, cur) => {
+  const buildoptfns = (helperfns) => 
+    helperfns.TAG_NAMES.reduce((hhh, cur) => {
       hhh[cur] = function () {
         let args = [].slice.call(arguments, 0),
             newargs;
@@ -75,15 +39,16 @@ let hyperscripthelpersopts = module.exports = (o => {
 
         return helperfns[cur].apply(0, newargs);
       };
+      
       return hhh;
     }, {});
-  };
 
-  const buildhelper = (helpers, tagnamearr) => ((h) => {
-    let namespace = buildoptfns(h, helpers, tagnamearr);
+  const buildhelper = (helpers) => ((h) => {
+    let helperobj = helpers(h),
+        namespace = buildoptfns(helperobj);
 
     let hhopts = opts => 
-      tagnamearr.reduce((hhopts, cur) => (
+      helperobj.TAG_NAMES.reduce((hhopts, cur) => (
         hhopts[cur] = function () {
           return namespace[cur](opts, ...arguments);
         },
@@ -93,14 +58,14 @@ let hyperscripthelpersopts = module.exports = (o => {
     hhopts.encodeid = encodeid;
     hhopts.decodeid = decodeid;
 
-    return tagnamearr.reduce((hhoptsfn, tagname) => (
+    return helperobj.TAG_NAMES.reduce((hhoptsfn, tagname) => (
       hhoptsfn[tagname] = namespace[tagname],
       hhoptsfn
     ), hhopts);
   });
 
-  o = buildhelper(hh, TAG_NAMES);
-  o.svg = buildhelper(hhsvg, TAG_NAMES_SVG);
+  o = buildhelper(hh);
+  o.svg = buildhelper(hhsvg);
 
   return o;
   
